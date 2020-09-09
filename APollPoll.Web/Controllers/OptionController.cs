2af86bf03627b137f1b560c6df5e52ab221f3e56
Joshua Tucker <the1jtux@gmail.com>
2020-09-08 23:54:53 -0400

    Added ability to add Options to Questions

---
+++ APollPoll.Web/Controllers/OptionController.cs
@@ -0,0 +1,36 @@
+﻿using APollPoll.Services.Options.Models;
+using APollPoll.Services.Options.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace APollPoll.Web.Controllers
+{
+    public class OptionController : Controller
+    {
+        private readonly IOptionService _service;
+        public OptionController(IOptionService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add(OptionCreate model)
+        {
+            if (!ModelState.IsValid)
+                return View(ModelState);
+
+            if (await _service.CreateAsync(model))
+                return RedirectToAction("Details", "Question", new { id = model.QuestionId });
+
+            ModelState.AddModelError("Save Failed", "Could not save the answer.");
+            return View(model);
+        }
+
+
+    }
+}
